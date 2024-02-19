b9e0d104de8262ebac2c3037a3a824666903768e
isuru <isuru2003a@gmail.com>
2024-02-19 20:10:32 +1100

    REgister world API

--- src/main/java/net/nonverse/labs/minecraft/httpendpoints/Api/ApiInitializer.java
+++ src/main/java/net/nonverse/labs/minecraft/httpendpoints/Api/ApiInitializer.java
@@ -3,13 +3,19 @@ package net.nonverse.labs.minecraft.httpendpoints.Api;
 public class ApiInitializer {
 
     private final ChatApi chatApi;
+    private final WorldApi worldApi;
 
     public ApiInitializer() {
         // Instantiate api handlers
         this.chatApi = new ChatApi();
+        this.worldApi = new WorldApi();
     }
 
     public ChatApi chatApi() {
         return this.chatApi;
+    }
+
+    public WorldApi worldApi() {
+        return this.worldApi;
     }
 }
