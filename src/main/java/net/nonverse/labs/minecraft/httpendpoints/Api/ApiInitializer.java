efcae567b3a2f42e66d577b6288cb94a2ad6abff
isuru <isuru2003a@gmail.com>
2024-02-22 02:48:07 +1100

    chatApi is now handled by Velocity plugin

--- src/main/java/net/nonverse/labs/minecraft/httpendpoints/Api/ApiInitializer.java
+++ src/main/java/net/nonverse/labs/minecraft/httpendpoints/Api/ApiInitializer.java
@@ -1,18 +1,11 @@
 package net.nonverse.labs.minecraft.httpendpoints.Api;
 
 public class ApiInitializer {
-
-    private final ChatApi chatApi;
     private final WorldApi worldApi;
 
     public ApiInitializer() {
         // Instantiate api handlers
-        this.chatApi = new ChatApi();
         this.worldApi = new WorldApi();
-    }
-
-    public ChatApi chatApi() {
-        return this.chatApi;
     }
 
     public WorldApi worldApi() {
