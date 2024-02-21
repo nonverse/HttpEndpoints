b8bbfd12838fce672fc1d2a6d8cdadc37b175e2b
isuru <isuru2003a@gmail.com>
2024-02-21 17:19:50 +1100

    Refactor

--- src/main/java/net/nonverse/labs/minecraft/httpendpoints/Api/ChatApi.java
+++ src/main/java/net/nonverse/labs/minecraft/httpendpoints/Api/ChatApi.java
@@ -2,10 +2,8 @@ package net.nonverse.labs.minecraft.httpendpoints.Api;
 
 import io.javalin.http.Context;
 import io.javalin.http.UnprocessableContentResponse;
-import net.kyori.adventure.audience.Audience;
 import net.kyori.adventure.text.Component;
 import net.nonverse.labs.minecraft.httpendpoints.Entities.Player;
-import org.bukkit.Bukkit;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Objects;
